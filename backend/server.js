bc11c7ceed2b51ea2266b90599fc9cda3565c620
Goitom <goitishgebrekidan@gmail.com>
2026-08-06 14:55:33 +0200

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.route.js";
-
+import path from "path"
 dotenv.config();
 const app = express();
+const __dirname=path.resolve()
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+}
+app.get(/.*/,(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+})
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
